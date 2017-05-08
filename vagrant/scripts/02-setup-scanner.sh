5f43bbb88abda67fa0c404a4ea971142dcd68d62
shearna <alexander.shearn@gmail.com>
2017-05-08 16:37:37 +0100

    Small script tweaks, not running puppet yet

--- vagrant/scripts/02-setup-scanner.sh
+++ vagrant/scripts/02-setup-scanner.sh
@@ -8,5 +8,5 @@ fi
 
 yum install -y oscap-scanner scap-security-guide
 
-echo 'sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_C2S --results results.xml --report /home/vagrant/reports/report.html /usr/share/xml/scap/ssg/content/ssg-centos7-ds.xml' > run-scap-scan.sh
-chmod +x run-scap-scan.sh
+echo 'sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_C2S --results results.xml --report /home/vagrant/reports/report.html /usr/share/xml/scap/ssg/content/ssg-centos7-ds.xml' > ~/run-scap-scan.sh
+chmod +x ~/run-scap-scan.sh
