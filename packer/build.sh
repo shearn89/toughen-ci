e2c368538f93a2e35d9cadf3db151b90ee49a16f
shearna <alexander.shearn@gmail.com>
2017-05-08 13:11:15 +0100

    Attempting to set up the box as a base box for vagrant

--- packer/build.sh
+++ packer/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+echo 'cleaning directory'
+rm -rf build
 echo 'checking syntax'
 packer validate *.json
 echo 'building'
---
+++ packer/scripts/00-setup-basebox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+curl -L -O https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+mkdir -m 0700 .ssh
+mv vagrant.pub .ssh
+cat .ssh/*.pub > .ssh/authorized_keys
+chmod 600 .ssh/*
+
+sudo sed -i '/UseDNS/ s/yes/no/' /etc/ssh/sshd_config
+sudo sed -i 's/#UseDNS/UseDNS/' /etc/ssh/sshd_config
+
+sudo mkdir -p /media/cdrom
+sudo mount /dev/cdrom /media/cdrom
+sudo sh /media/cdrom/VBoxLinuxAdditions.run
+sudo umount /media/cdrom
