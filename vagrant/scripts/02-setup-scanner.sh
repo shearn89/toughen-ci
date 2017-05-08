c9f8a6b5af094b34bca8c155c2a28a28329f8f69
shearna <alexander.shearn@gmail.com>
2017-05-08 16:17:43 +0100

    Calling scripts from vagrantfile

--- vagrant/scripts/02-setup-scanner.sh
+++ vagrant/scripts/02-setup-scanner.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo yum install -y oscap-scanner scap-security-guide
+if [[ $EUID != 0 ]]
+then
+    echo 'must run as root'
+    exit 1
+fi
+
+yum install -y oscap-scanner scap-security-guide
 
 echo 'sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_C2S --results results.xml --report /home/vagrant/reports/report.html /usr/share/xml/scap/ssg/content/ssg-centos7-ds.xml' > run-scap-scan.sh
 chmod +x run-scap-scan.sh
--- vagrant/scripts/03-run-puppet.sh
+++ vagrant/scripts/03-run-puppet.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $EUID != 0 ]]
+then
+    echo 'must run as root'
+    exit 1
+fi
+
 r10k puppetfile purge
 r10k puppetfile install
 
