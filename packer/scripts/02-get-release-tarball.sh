caf21c928c894c3e589d1306627bd715d439447b
shearna <alexander.shearn@gmail.com>
2017-05-08 12:07:56 +0100

    Adding vagrantfile

--- packer/scripts/02-get-release-tarball.sh
+++ packer/scripts/02-get-release-tarball.sh
@@ -4,6 +4,6 @@ curl -O https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
 chmod +x jq-linux64
 sudo mv jq-linux64 /usr/bin/jq
 
-RELEASE_URL=$(curl https://api.github.com/repos/shearn89/puppet-toughen/tags | jq --raw-output '.[0].tarball_url')
+RELEASE_URL=$(curl -L https://api.github.com/repos/shearn89/puppet-toughen/tags | jq --raw-output '.[0].tarball_url')
 curl -s -O -J -L $RELEASE_URL
 
