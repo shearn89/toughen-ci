6fab3b6905b91fb1da0799b30ef09041600a1489
shearna <alexander.shearn@gmail.com>
2017-05-08 11:50:49 +0100

    Making release download part of the vagrant stage

--- packer/scripts/02-get-release-tarball.sh
+++ packer/scripts/02-get-release-tarball.sh
@@ -2,7 +2,7 @@
 
 curl -O https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
 chmod +x jq-linux64
-mv jq-linux64 /usr/bin/jq
+sudo mv jq-linux64 /usr/bin/jq
 
 RELEASE_URL=$(curl https://api.github.com/repos/shearn89/puppet-toughen/tags | jq --raw-output '.[0].tarball_url')
 curl -s -O -J -L $RELEASE_URL
