01b119afa14fdc640582b61e7f87a57e7b0ada0f
shearna <alexander.shearn@gmail.com>
2017-05-12 16:13:36 +0100

    Adding scan script and updated policy

---
+++ vagrant/scripts/run-scap-scan.sh
@@ -0,0 +1,1 @@
+sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_toughen --results results.xml --report /home/vagrant/reports/report-toughen.html /home/vagrant/scripts/toughen.xml
